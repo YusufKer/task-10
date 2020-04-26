aa51522063c9ba8617ff9d2d3087bb60604d49c2
YusufKer <53224370+YusufKer@users.noreply.github.com>
2020-04-26 13:31:03 +0200

    Updates

---
+++ precache-manifest.0f3a13a03d1611675439386e45b5a917.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b2acb9004419fd0721cf12639ccf647",
+    "url": "/task-10/index.html"
+  },
+  {
+    "revision": "750b666d884498ec1eb0",
+    "url": "/task-10/static/css/main.b9a1a91e.chunk.css"
+  },
+  {
+    "revision": "f2acf38c915e5d8f51b8",
+    "url": "/task-10/static/js/2.2119ba05.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/task-10/static/js/2.2119ba05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "750b666d884498ec1eb0",
+    "url": "/task-10/static/js/main.34f3aa98.chunk.js"
+  },
+  {
+    "revision": "d26b527cd0c2c7630c5a",
+    "url": "/task-10/static/js/runtime-main.7d711fbe.js"
+  },
+  {
+    "revision": "c58d5048b47db872c1232429e6f8d806",
+    "url": "/task-10/static/media/devil.c58d5048.svg"
+  },
+  {
+    "revision": "54e95e63b4aee7e10e6ce986c43d53b4",
+    "url": "/task-10/static/media/spider.54e95e63.svg"
+  }
+]);
